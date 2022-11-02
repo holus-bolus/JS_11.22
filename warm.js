107d02ebd59f38b29dd7aa3e62d7da631312d30b
holus-bolus <ustinovoleksij@gmail.com>
2022-11-02 13:06:24 +0200

    Added files

--- warm.js
+++ warm.js
@@ -1,23 +1,41 @@
-function warmOrCold() {
-    const FIRST_NUMBER = prompt('Enter a first number that is bigger than zero');
-    const SECOND_NUMBER = prompt('Enter a second number that is bigger than zero');
-    let firstNumberStorage = '';
-    let secondNumberStorage = '';
+    let iterationStorage = '';
 
-    firstNumberStorage += FIRST_NUMBER;
-    secondNumberStorage += SECOND_NUMBER;
-    if (+FIRST_NUMBER < 0 || +SECOND_NUMBER < 0) {
-        alert('Enter only positive numbers!');
-    } else if ((+SECOND_NUMBER + 100) < +FIRST_NUMBER) {
-        alert(`Please enter a number greater than ${+SECOND_NUMBER + 100}`);
+    function askFirstNumber() {
+        let firstNumberStorage = parseInt('');
+        let secondNumberStorage = parseInt('');
+        while (true) {
+            const ASK_FIRST_NUMBER = prompt("Enter a first number");
+            firstNumberStorage += ASK_FIRST_NUMBER;
+            if (ASK_FIRST_NUMBER < 0) {
+                alert("Please enter only positive numbers");
+                continue;
+            } else if (isNaN(+ASK_FIRST_NUMBER)) {
+                alert("Please enter a number");
+                continue;
+            }
+            const ASK_SECOND_NUMBER = prompt("Enter a second number that bigger than first number for 100 points(`${+firstNumberStorage+100}`)");
+            secondNumberStorage += ASK_SECOND_NUMBER;
+            if (+ASK_SECOND_NUMBER < 0) {
+                alert("Please enter only positive numbers");
+            } else if (isNaN(+ASK_SECOND_NUMBER)) {
+                alert("Please enter a number");
+                continue;
+            } else if ((+ASK_SECOND_NUMBER + 100) < firstNumberStorage) {
+                alert(`Please enter a number greater than ${firstNumberStorage + 100}`)
+            }
+            let randomNumber = firstNumberStorage - 0.5 + Math.random() * (secondNumberStorage - firstNumberStorage + 1);
+            let guessesStorage = parseInt('');
+            const USER_GUESS = prompt("Enter a number and try to guess what number was conceived by the program");
+            if (+USER_GUESS === randomNumber) {
+                alert("Great! It'\s like you knew the number")
+            } else if (+USER_GUESS < randomNumber) {
+                alert("Cold");
+                continue;
+            } else if (+USER_GUESS - 1 < randomNumber) {
+                alert("Colder");
+                continue;
+            } else if ()
+                }
     }
 
-    let randomNumber = FIRST_NUMBER + Math.random() * (SECOND_NUMBER + 1 - FIRST_NUMBER);
-    let integerNumber = Math.trunc(+randomNumber);
-    const THIRD_NUMBER = +prompt('Enter a third number');
-    if (THIRD_NUMBER === integerNumber) {
-
-    }
-}
-
-warmOrCold();
+    askFirstNumber();
