856842bcb5fe908442141eb721f8e907b533bb0d
holus-bolus <ustinovoleksij@gmail.com>
2022-11-02 22:23:29 +0200

    Tas 1:[added the askuser function

--- Second_task/script.js
+++ Second_task/script.js
@@ -1,17 +1,20 @@
 function consoleCube() {
-    const ASK_SYMBOLS = prompt('Enter any symbols, no more than 3 symbols');
-    const ASK_NUMBER = prompt('Enter a number less than 10');
-
     let symbolsStorage = '';
-    if (ASK_SYMBOLS.length > 3 || ASK_NUMBER > 10 || ASK_NUMBER < 0 || !isNaN(+ASK_SYMBOLS) || ASK_SYMBOLS === '' || ASK_NUMBER === '') {
+    const ASK_SYMBOLS = prompt('Enter any symbols, no more than 3 symbols');
+    if(ASK_SYMBOLS.length > 3|| !isNaN(+ASK_SYMBOLS) || ASK_SYMBOLS === ''){
+        console.log('Incorrect input!');
+        return symbolsStorage;
+    }
+    const ASK_NUMBER = prompt('Enter a number less than 10');
+    if ( ASK_NUMBER > 10 || ASK_NUMBER < 0 || ASK_NUMBER === '') {
         console.log('Incorrect input!');
         return symbolsStorage;
     }
     for (let iterator = 0; iterator < ASK_NUMBER; iterator++) {
-        for (let counter = 0; counter < ASK_NUMBER; counter++) {
+        for (let counter = 0; counter <ASK_NUMBER; counter++) {
             symbolsStorage += `${ASK_SYMBOLS}`;
         }
-        symbolsStorage += '\n';
+        symbolsStorage+='\n';
     }
     console.log(symbolsStorage);
 }
