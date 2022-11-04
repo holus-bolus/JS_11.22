02fe589b8d1cb4faa46b6dd4a4d01ba1400efbe7
holus-bolus <ustinovoleksij@gmail.com>
2022-11-04 10:04:02 +0200

    [Task_2]: Added the function for interval

--- JS-second-hw/Select-From-interval/interval.js
+++ JS-second-hw/Select-From-interval/interval.js
@@ -1,7 +1,18 @@
 function selectFromInterval(array, firstNumber, secondNumber) {
     const ARRAY_OF_VALUES = [];
-    if (!Array.isArray(array)||isNaN(firstNumber)||isNaN(secondNumber)) {
+    if (!Array.isArray(array) || isNaN(firstNumber) || isNaN(secondNumber)) {
         throw new Error('Please check the entry parameters');
     }
-
+    let number;
+    if (firstNumber > secondNumber) {
+        number = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = number;
+    }
+    for (let iterator = 0; iterator < array.length; iterator++) {
+        if (array[iterator] >= firstNumber && array[iterator] <= secondNumber) {
+           ARRAY_OF_VALUES.push(array[iterator]);
+        }
+    }
+    return ARRAY_OF_VALUES;
 }
