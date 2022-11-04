6f4517c4bb29e01267fdcb01d8563c1fdf8a23ed
holus-bolus <ustinovoleksij@gmail.com>
2022-11-04 09:37:17 +0200

    [Second_task]:Added a function for a deep copying object

---
+++ JS-second-hw/Deep-copy/index.js
@@ -0,0 +1,18 @@
+        function makeDeepCopy(object) {
+            if (typeof object !== 'object' || object === null || Array.isArray(object)) {
+                throw new Error('It\'s not an object');
+            }
+            let clonedObject = {};
+            for (let iterator in object) {
+                if (typeof (object[iterator]) == 'object' && object[iterator] !== null) {
+                    clonedObject [iterator] = makeDeepCopy(object[iterator]);
+                } else {
+                    clonedObject [iterator] = object[iterator];
+                }
+            }
+            return clonedObject;
+        }
+
+
+
+
---
+++ JS-second-hw/Select-From-interval/interval.js
@@ -0,0 +1,7 @@
+function selectFromInterval(array, firstNumber, secondNumber) {
+    const ARRAY_OF_VALUES = [];
+    if (!Array.isArray(array)||isNaN(firstNumber)||isNaN(secondNumber)) {
+        throw new Error('Please check the entry parameters');
+    }
+
+}
