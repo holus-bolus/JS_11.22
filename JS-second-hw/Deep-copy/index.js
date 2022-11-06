02e326418ba00c76dc4e29ba9e27a90a1eaaa96d
holus-bolus <ustinovoleksij@gmail.com>
2022-11-06 23:03:25 +0200

    Added a fucntion for deep copying objects

--- JS-second-hw/Deep-copy/index.js
+++ JS-second-hw/Deep-copy/index.js
@@ -1,14 +1,27 @@
+/**
+ *
+ * @param {Object|Array} object
+ * @returns {Object|Array}
+ */
+
 function makeDeepCopy(object) {
-    if (typeof object !== 'object' || object === null) {
-        throw new Error('It\'s not an object');
+  if (typeof object !== 'object' || object === null || object === 'undefined') {
+    throw new Error("It's not an object");
+  }
+  if (Array.isArray(object)) {
+    return object.map((item) => {
+      if (item instanceof Object) return makeDeepCopy(item);
+      return item;
+    });
+  }
+  const clonedObject = {};
+  Object.assign(clonedObject, object);
+  Object.entries(object).forEach(([key, value]) => {
+    if (value instanceof Object) {
+      clonedObject[key] = makeDeepCopy(value);
+    } else {
+      clonedObject[key] = value;
     }
-    let clonedObject = {};
-    for (let iterator in object) {
-        if (typeof (object[iterator]) == 'object' && object[iterator] !== null) {
-            clonedObject [iterator] = makeDeepCopy(object[iterator]);
-        } else {
-            clonedObject [iterator] = object[iterator];
-        }
-    }
-    return clonedObject;
+  });
+  return clonedObject;
 }
