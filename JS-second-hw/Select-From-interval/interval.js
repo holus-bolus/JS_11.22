f6b979527aa20cd819468c66b7989efb848de36e
holus-bolus <ustinovoleksij@gmail.com>
2022-11-06 23:05:15 +0200

    Added a function for creating an object from the interval

--- JS-second-hw/Select-From-interval/interval.js
+++ JS-second-hw/Select-From-interval/interval.js
@@ -1,18 +1,23 @@
-function selectFromInterval(array, firstNumber, secondNumber) {
-    const ARRAY_OF_VALUES = [];
-    if (!Array.isArray(array) || isNaN(firstNumber) || isNaN(secondNumber)) {
-        throw new Error('Please check the entry parameters');
+/**
+ * @param {number[]} array
+ * @param {number} from
+ * @param {number} to
+ * @returns {number[]}
+ */
+function selectFromInterval(array, from, to) {
+  if (!Array.isArray(array) || isNaN(from) || isNaN(to)) {
+    throw new Error('Please check the entry parameters');
+  }
+  if (from > to) {
+    const copiedFrom = from;
+
+    from = to;
+    to = copiedFrom;
+  }
+  return array.filter((number) => {
+    if (typeof number !== 'number') {
+      throw new Error('The array should have only numbers');
     }
-    let number;
-    if (firstNumber > secondNumber) {
-        number = firstNumber;
-        firstNumber = secondNumber;
-        secondNumber = number;
-    }
-    for (let iterator = 0; iterator < array.length; iterator++) {
-        if (array[iterator] >= firstNumber && array[iterator] <= secondNumber) {
-            ARRAY_OF_VALUES.push(array[iterator]);
-        }
-    }
-    return ARRAY_OF_VALUES
+    return number >= from && number <= to;
+  });
 }
