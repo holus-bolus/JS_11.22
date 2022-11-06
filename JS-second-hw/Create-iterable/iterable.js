3bfe0626bf8ae60d9f2058092e09ec5f3962ccf8
holus-bolus <ustinovoleksij@gmail.com>
2022-11-06 23:02:32 +0200

    Added a function for creating objects

--- JS-second-hw/Create-iterable/iterable.js
+++ JS-second-hw/Create-iterable/iterable.js
@@ -1,24 +1,31 @@
-function createIterable(start, end) {
-    if ((start === false || end === false) || (isNaN(+start)) || isNaN(+end) || (end <= start)) {
-        throw new Error();
-    }
-    let range = {
-        from: start,
-        to: end,
-
-        [Symbol.iterator]() {
-            this.current = this.from;
-            return this;
-        },
-        next() {
-            if (this.current <= this.to) {
-                return {done: false, value: this.current++};
-            } else {
-                return {done: true};
-            }
-        }
-    }
-    return range;
+/**
+ * @param {number} from
+ * @param {number} to
+ * @returns {{done: boolean, value: number}|{done: boolean}|{next(): ({done: boolean, value}), [Symbol.iterator](): this, from, to}|*}
+ */
+function createIterableObject(from, to) {
+  if (
+    from === undefined ||
+    to === undefined ||
+    Number.isNaN(from) ||
+    Number.isNaN(to) ||
+    to <= from
+  ) {
+    throw new Error();
+  }
+  return {
+    from,
+    to,
+    [Symbol.iterator]() {
+      this.current = this.from;
+      return this;
+    },
+    next() {
+      if (this.current <= this.to) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    },
+  };
 }
-
-
