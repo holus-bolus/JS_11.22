0cd24d089b3647cf9a31b567adbdc551ac49c217
holus-bolus <ustinovoleksij@gmail.com>
2022-11-22 18:50:13 +0200

    [Task 6: added a calculator]

--- src/script.js
+++ src/script.js
@@ -1,131 +1,357 @@
-const display1El = document.querySelector('.display-1');
-const display2El = document.querySelector('.display-2');
-const tempResultEl = document.querySelector('.temp-result');
-const numbersEl = document.querySelectorAll('.number');
-const operationEl = document.querySelectorAll('.operation');
-const equalEl = document.querySelector('.equal');
-const clearAllEl = document.querySelector('.all-clear');
-const clearLastEl = document.querySelector('.last-entity-clear');
-let dis1Num = '';
-let dis2Num = '';
-let result = null;
-let lastOperation = '';
-let haveDot = false;
+document.addEventListener('DOMContentLoaded', app);
 
-numbersEl.forEach((number) => {
-  number.addEventListener('click', (e) => {
-    if (e.target.innerText === '.' && !haveDot) {
-      haveDot = true;
-    } else if (e.target.innerText === '.' && haveDot) {
-      return;
-    }
-    dis2Num += e.target.innerText;
-    display2El.innerText = dis2Num;
-  });
-});
+function app() {
+  let calc = document.forms[0],
+    isCalcing = false,
+    error = '',
+    val = 0,
+    memory = 0,
+    op = '',
+    exp = [],
+    lastPart = '',
+    lastOperand = 0,
+    signs = /[\/\*\-\+]/,
+    debug = false,
+    action = (e) => {
+      let tar = e.target || e,
+        fn = fnByKey(e.keyCode, e.shiftKey, e.altKey, e.metaKey),
+        output = calc.output;
+      if (e.keyCode === 13) e.preventDefault();
+      else if (!fn && !e.keyCode) fn = tar.getAttribute('data-fn');
+      if (fn && output) {
+        let maxLen = +output.maxLength,
+          maxVal = 10 ** maxLen,
+          fnIsNum = !isNaN(fn),
+          fnIsDec = fn === '.',
+          fnIsPct = fn === '%',
+          fnIsSqrt = fn === 'sqrt',
+          fnIsSign = fn === '+-',
+          recallOrClearM = fn === 'mrc',
+          subFromM = fn === 'm-',
+          addToM = fn === 'm+',
+          fnIsOp = '/*-+'.indexOf(fn) > -1,
+          fnIsEquals = fn === '=',
+          fnIsClear = fn === 'C';
+        if (!error) {
+          if (exp.length) lastPart = exp[exp.length - 1];
+          if (fnIsNum || fnIsDec || fnIsPct || fnIsSqrt || fnIsSign || fnIsOp) {
+            if (!isCalcing) {
+              isCalcing = true;
+              if (fnIsNum || fnIsDec) {
+                op = '';
+                exp = ['0'];
+                lastPart = exp[0];
+                lastOperand = 0;
+              }
+            }
+            if (!exp.length) exp.push('0');
+          }
+          if (fnIsNum) {
+            if (isNaN(lastPart)) {
+              exp.push(fn);
+            } else if (lastPart.length < maxLen) {
+              let numToAdd = lastPart + fn;
+              if (numToAdd[0] === '0' && numToAdd[1] !== '.')
+                numToAdd = numToAdd.substr(1);
+              exp[exp.length - 1] = numToAdd;
+            }
+          } else if (fnIsDec) {
+            if (lastPart.indexOf('.') === -1) {
+              if (isNaN(lastPart)) exp.push('0.');
+              else if (lastPart.length < maxLen - 1) exp[exp.length - 1] += fn;
+            }
+          } else if (fnIsPct) {
+            if (exp.length) {
+              if (!isNaN(lastPart)) {
+                if (op && exp.indexOf(op) > -1) {
+                  exp[exp.length - 1] = nearestLastDecPt(
+                    (exp[0] * exp[exp.length - 1]) / 100
+                  );
+                } else {
+                  exp[exp.length - 1] = nearestLastDecPt(
+                    exp[exp.length - 1] / 100
+                  );
+                  isCalcing = false;
+                }
+              } else {
+                exp.push(`${exp[0] * (exp[0] / 100)}`);
+                exp[exp.length - 1] = nearestLastDecPt(exp[exp.length - 1]);
+              }
+            }
+            blink();
+          } else if (fnIsSqrt) {
+            if (exp.length) {
+              if (!isNaN(lastPart))
+                exp[exp.length - 1] = String(Math.sqrt(exp[exp.length - 1]));
+              else exp.push(String(Math.sqrt(exp[0])));
+            }
+            blink();
+          } else if (fnIsSign) {
+            if (exp.length) {
+              if (!isNaN(lastPart))
+                exp[exp.length - 1] = String(-exp[exp.length - 1]);
+              else exp[0] = String(-exp[0]);
+            }
+          } else if (recallOrClearM) {
+            if (memory !== 0 && memory === val) {
+              if (exp.length <= 1) {
+                isCalcing = false;
+                memory = 0;
+                displayM(false);
+              }
+            } else {
+              if (!isCalcing) isCalcing = true;
+              if (isNaN(lastPart) || !exp.length) exp.push(String(memory));
+              else exp[exp.length - 1] = String(memory);
+              lastPart = exp[exp.length - 1];
+              lastOperand = lastPart;
+              displayM(memory !== 0);
+            }
+          } else if (fnIsOp) {
+            op = fn;
+            if (isNaN(lastPart)) {
+              exp[exp.length - 1] = op;
+            } else {
+              let curExp = exp.join(' ');
+              if (signs.test(curExp)) {
+                exp = [`${solve(exp[0], exp[1], exp[2])}`];
+                exp[0] = nearestLastDecPt(exp[0]);
+                lastPart = exp[0];
+                val = lastPart;
+              }
+              exp.push(op);
+            }
+            blink();
+          } else {
+            let memAction = subFromM || addToM;
+            if (fnIsEquals || memAction) {
+              isCalcing = false;
+              if (op && exp.indexOf(op) > -1) lastOperand = lastPart;
+              let compoundEquals = lastOperand && exp.indexOf(op) === -1,
+                normalEquals = !isNaN(lastOperand);
+              if (compoundEquals) {
+                if (!memAction) exp = [`${solve(val, op, lastOperand)}`];
+              } else if (normalEquals) {
+                exp = [`${solve(exp[0], op, lastOperand)}`];
+              } else {
+                lastOperand = exp[0];
+                exp = [`${solve(val, op, val)}`];
+              }
+              exp[0] = nearestLastDecPt(exp[0]);
+              if (memAction) {
+                if (subFromM) memory -= +exp[0];
+                if (addToM) memory += +exp[0];
 
-operationEl.forEach((operation) => {
-  operation.addEventListener('click', (e) => {
-    if (!dis2Num) return;
-    haveDot = false;
-    const operationName = e.target.innerText;
-    if (dis1Num && dis2Num && lastOperation) {
-      mathOperation();
-    } else {
-      result = parseFloat(dis2Num);
-    }
-    clearVar(operationName);
-    lastOperation = operationName;
-  });
-});
+                displayM(memory !== 0);
+                if (debug) console.log(`M: ${m}`);
+              }
+              blink();
+            }
+          }
+          if (fn !== 'C') {
+            lastPart = exp[exp.length - 1];
+            if (!isNaN(lastPart) || lastPart === 'NaN') val = lastPart;
+            if (Math.abs(val) === Infinity || val === 'NaN') {
+              error = '0';
+            } else if (val <= -maxVal / 10) {
+              let cutNeg = maxLen - 3;
+              if (cutNeg < 0) cutNeg = 0;
+              error = (val / maxVal).toFixed(cutNeg);
+            } else if (val >= maxVal) {
+              let cutPos = maxLen - 2;
+              if (cutPos < 0) cutPos = 0;
+              error = (val / maxVal).toFixed(cutPos);
+            }
+            let outputVal = error || String(val);
+            if (!error) {
+              if ((val >= -1e8 && val < -1e7) || (val >= 1e8 && val < 1e9))
+                outputVal = outputVal.substr(0, maxLen - 1);
+              else outputVal = outputVal.substr(0, maxLen);
+            } else {
+              displayE();
+            }
+            output.value = outputVal;
+          }
+        }
+        if (fnIsClear) {
+          if (debug) console.clear();
+          isCalcing = false;
+          error = '';
+          val = 0;
+          if (Math.abs(memory) === Infinity || isNaN(memory)) {
+            memory = 0;
+            displayM(false);
+          }
+          op = '';
+          exp = [];
+          lastPart = '';
+          lastOperand = 0;
+          output.value = val;
+          blink();
+          displayE(false);
+        }
+        if (debug) console.log(exp);
+      }
+    },
+    blink = () => {
+      let output = calc.output;
+      if (output) {
+        let fadeInClass = 'calc__screen--fade-in',
+          screenCL = output.classList;
 
-function clearVar(name = '') {
-  dis1Num += dis2Num + ' ' + name + ' ';
-  display1El.innerText = dis1Num;
-  display2El.innerText = '';
-  dis2Num = '';
-  tempResultEl.innerText = result;
-}
+        screenCL.remove(fadeInClass);
+        void output.offsetWidth;
+        setTimeout(() => {
+          screenCL.add(fadeInClass);
+        }, 0);
+      }
+    },
+    displayE = (show = true) => {
+      let ce = calc.querySelector('.calc__error');
+      ce.textContent = show ? 'E' : '';
+    },
+    displayM = (show = true) => {
+      let cm = calc.querySelector('.calc__memory');
+      cm.textContent = show ? 'M' : '';
+    },
+    fnByKey = (keycode, isShift, isAlt, isCmd) => {
+      let fn = '';
+      switch (keycode) {
+        case 12:
+        case 27:
+        case 67:
+          if (!isCmd) fn = 'C';
+          break;
+        case 13:
+          fn = '=';
+          break;
+        case 48:
+        case 96:
+          fn = '0';
+          break;
+        case 49:
+        case 97:
+          fn = '1';
+          break;
+        case 50:
+        case 98:
+          fn = '2';
+          break;
+        case 51:
+        case 99:
+          fn = '3';
+          break;
+        case 52:
+        case 100:
+          fn = '4';
+          break;
+        case 53:
+        case 101:
+          fn = isShift ? '%' : '5';
+          break;
+        case 54:
+        case 102:
+          fn = '6';
+          break;
+        case 55:
+        case 103:
+          fn = '7';
+          break;
+        case 56:
+          fn = isShift ? '*' : '8';
+          break;
+        case 104:
+          fn = '8';
+          break;
+        case 57:
+        case 105:
+          fn = '9';
+          break;
+        case 77:
+          fn = 'mrc';
+          break;
+        case 83:
+          fn = 'sqrt';
+          break;
+        case 106:
+          fn = '*';
+          break;
+        case 107:
+          fn = '+';
+          break;
+        case 187:
+          fn = isShift ? '+' : '=';
+          break;
+        case 188:
+          fn = isShift ? 'm-' : '';
+          break;
+        case 109:
+        case 189:
+          fn = isAlt ? '+-' : '-';
+          break;
+        case 110:
+          fn = '.';
+          break;
+        case 190:
+          fn = isShift ? 'm+' : '.';
+          break;
+        case 111:
+        case 191:
+          fn = '/';
+          break;
+        default:
+          fn = '';
+          break;
+      }
+      return fn;
+    },
+    kbdButtonPress = (e, state = 'down') => {
+      let tar = e.target || e,
+        fn = fnByKey(e.keyCode, e.shiftKey, e.altKey, e.metaKey),
+        key = calc.querySelector(`[data-fn="${fn}"]`);
 
-function mathOperation() {
-  if (lastOperation === 'x') {
-    result = parseFloat(result) * parseFloat(dis2Num);
-  } else if (lastOperation === '+') {
-    result = parseFloat(result) + parseFloat(dis2Num);
-  } else if (lastOperation === '-') {
-    result = parseFloat(result) - parseFloat(dis2Num);
-  } else if (lastOperation === '/') {
-    result = parseFloat(result) / parseFloat(dis2Num);
-  } else if (lastOperation === '%') {
-    result = parseFloat(result) % parseFloat(dis2Num);
-  }
-}
+      if (key) {
+        let activeClass = 'calc__btn--active',
+          keyCL = key.classList;
 
-equalEl.addEventListener('click', () => {
-  if (!dis2Num || !dis1Num) return;
-  haveDot = false;
-  mathOperation();
-  clearVar();
-  display2El.innerText = result;
-  tempResultEl.innerText = '';
-  dis2Num = result;
-  dis1Num = '';
-});
+        if (state === 'down' && !keyCL.contains(activeClass))
+          keyCL.add(activeClass);
+        else if (state === 'up' && keyCL.contains(activeClass))
+          keyCL.remove(activeClass);
+      }
+    },
+    nearestLastDecPt = (n, places = 9) => {
+      let power = 10 ** -places,
+        r = Math.round(n / power) * power;
+      return String(+r.toFixed(places));
+    },
+    solve = (A, op, B) => {
+      let r = 0,
+        a = !isNaN(A) ? +A : r,
+        b = !isNaN(B) ? +B : a;
 
-clearAllEl.addEventListener('click', () => {
-  dis1Num = '';
-  dis2Num = '';
-  display1El.innerText = '';
-  display2El.innerText = '';
-  result = '';
-  tempResultEl.innerText = '';
-});
+      switch (op) {
+        case '/':
+          r = a / b;
+          break;
+        case '*':
+          r = a * b;
+          break;
+        case '-':
+          r = a - b;
+          break;
+        case '+':
+          r = a + b;
+          break;
+        default:
+          r = a;
+          break;
+      }
+      return r;
+    };
 
-clearLastEl.addEventListener('click', () => {
-  display2El.innerText = '';
-  dis2Num = '';
-});
-
-window.addEventListener('keydown', (e) => {
-  if (
-    e.key === '0' ||
-    e.key === '1' ||
-    e.key === '2' ||
-    e.key === '3' ||
-    e.key === '4' ||
-    e.key === '5' ||
-    e.key === '6' ||
-    e.key === '7' ||
-    e.key === '8' ||
-    e.key === '9' ||
-    e.key === '.'
-  ) {
-    clickButtonEl(e.key);
-  } else if (e.key === '+' || e.key === '-' || e.key === '/' || e.key === '%') {
-    clickOperation(e.key);
-  } else if (e.key === '*') {
-    clickOperation('x');
-  } else if (e.key === '=') {
-    clickEqual();
-  }
-});
-
-function clickButtonEl(key) {
-  numbersEl.forEach((button) => {
-    if (button.innerText === key) {
-      button.click();
-    }
-  });
-}
-
-function clickOperation(key) {
-  operationEl.forEach((operation) => {
-    if (operation.innerText === key) {
-      operation.click();
-    }
-  });
-}
-
-function clickEqual() {
-  equalEl.click();
+  calc.addEventListener('click', action);
+  document.addEventListener('keydown', action);
+  document.addEventListener('keydown', kbdButtonPress);
+  document.addEventListener('keyup', (e) => kbdButtonPress(e, 'up'));
 }
